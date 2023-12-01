4dcf15e92dc47cff21d31bb90fbb09a039374319
SiweiTang <siweitang@hotmail.com>
2023-12-01 09:49:32 -0500

    v30: encode pwd

---
+++ shopmecommon/src/main/java/com/shopme/common/entity/Role.java
@@ -0,0 +1,84 @@
+package com.shopme.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 40, nullable = false, unique = true)
+    private String name;
+
+    public Role(){}
+
+    public Role(Integer id) {
+		this.id = id;
+	}
+
+	public Role(String name) {
+		this.name = name;
+	}
+
+    public Role(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    @Column(length = 150, nullable = false)
+    private String description;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+
+}
