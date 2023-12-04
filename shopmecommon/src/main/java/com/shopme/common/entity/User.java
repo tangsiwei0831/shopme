acd585fa8e2442584013ee1884c37f391309b1d9
SiweiTang <siweitang@hotmail.com>
2023-12-04 16:55:17 -0500

    v36: image upload and display

--- shopmecommon/src/main/java/com/shopme/common/entity/User.java
+++ shopmecommon/src/main/java/com/shopme/common/entity/User.java
@@ -13,6 +13,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "user")
@@ -129,4 +130,12 @@ public class User {
 		return "User [id=" + id + ", email=" + email + ", firstName=" + firstName + ", lastName=" + lastName
 				+ ", roles=" + roles + "]";
 	}
+
+    @Transient
+    public String getPhotosImagePath(){
+        if(id == null || photos == null) {
+            return "/images/default-user.png";
+        };
+        return "/user-photos/" + this.id + "/" + this.photos;
+    }
 }
