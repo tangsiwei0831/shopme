ad6157954ee8fcf4243d04b29dfda1f1ed5beb81
SiweiTang <siweitang@hotmail.com>
2023-12-05 15:51:04 -0500

    v42: filter function

--- shopmewebparent/shopmebackend/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
+++ shopmewebparent/shopmebackend/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
@@ -129,4 +129,17 @@ public class UserRepositoryTests {
 		listUsers.forEach(user-> System.out.println(user));
 		assertThat(listUsers.size()).isEqualTo(pageSize);
 	}
+
+	@Test
+	public void TestSearchUsers(){
+		String keyword = "bruce";
+
+		int pageNumber = 0;
+		int pageSize = 4;
+		Pageable pageable = PageRequest.of(pageNumber, pageSize);
+		Page<User> page = repo.findAll(keyword, pageable);
+		List<User> listUsers = page.getContent();
+		listUsers.forEach(user-> System.out.println(user));
+		assertThat(listUsers.size()).isGreaterThan(0);
+	}
 }
