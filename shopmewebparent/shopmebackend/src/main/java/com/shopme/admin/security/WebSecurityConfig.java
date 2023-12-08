71df2df1f30e740dba338ba32151dc2dbae838d3
SiweiTang <siweitang@hotmail.com>
2023-12-08 11:50:00 -0500

    v51: code user log in

--- shopmewebparent/shopmebackend/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ shopmewebparent/shopmebackend/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -2,9 +2,13 @@ package com.shopme.admin.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,10 +17,31 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+	@Bean
+	public UserDetailsService userDetailsService(){
+		return new ShopmeUserDetailsService();
+	}
+
 	@Bean
 	public PasswordEncoder passwordEncoder(){
 		return new BCryptPasswordEncoder();
 	}
+
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(passwordEncoder());
+		
+		return authProvider;
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception{
+		return authConfig.getAuthenticationManager();
+	}
+
 
     @Bean
 	protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
